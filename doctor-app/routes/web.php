01e383cdf600e0499c358fb1c2866a1e5f05ffff
Anastasija <anastasija.krstanoska@students.finki.ukim.mk>
2024-02-03 22:22:54 +0100

    Implemented Create Doctor.

--- doctor-app/routes/web.php
+++ doctor-app/routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\DoctorController;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
@@ -21,3 +22,21 @@ Route::get('/', function () {
 Route::get('/dashboard', function () {
     return view('dashboard');
 });
+
+
+Auth::routes();
+
+//Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+//Route::get('/doctor', [DoctorController::class,'create']);
+
+//Route::resource('doctor', 'DoctorController');
+
+Route::group(['prefix' => 'doctors'], function () {
+    Route::get('/', [DoctorController::class, 'index']); //site users                OK
+    Route::get('/show/{id?}', [DoctorController::class, 'show']);
+    Route::get('/edit/{id?}', [DoctorController::class, 'show']);
+    Route::post('/store', [DoctorController::class, 'store'])->name('doctor.store');
+    Route::get('/create', [DoctorController::class, 'create']);
+    Route::post('/delete/{id}', [DoctorController::class, 'destroy']);
+});
+
