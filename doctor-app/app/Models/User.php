8d6a351545fd54ad37b5518ffa91c6e7b8627d86
Anastasija <anastasija.krstanoska@students.finki.ukim.mk>
2024-02-08 11:53:17 +0100

    Implemented Create Doctor.

--- doctor-app/app/Models/User.php
+++ doctor-app/app/Models/User.php
@@ -18,9 +18,7 @@ class User extends Authenticatable
      * @var array<int, string>
      */
     protected $fillable = [
-        'name',
-        'email',
-        'password',
+        'name', 'email', 'password', 'gender', 'education', 'address', 'department', 'phone_number', 'image', 'role_id', 'description'
     ];
 
     /**
