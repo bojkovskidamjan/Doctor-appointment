1f78dae4ea28c12bf8d96c0515292980730acce8
ivanovskipetar <ivanovskipetar4@gmail.com>
2024-03-10 15:51:48 +0100

    Final design of welcome page.

--- doctor-app/resources/views/welcome.blade.php
+++ doctor-app/resources/views/welcome.blade.php
@@ -3,11 +3,11 @@
 @section('content')
     <div class="container">
         <div class="row">
-           <div class="col-md-6">
-               <img src="/banner/doctor-banner.jpg" class="img-fluid" style="border:1px solid #ccc; ">
-           </div>
             <div class="col-md-6">
-               <h2>Create an account & Book your appointment</h2>
+                <img src="/banner/doctor-banner.jpg" class="img-fluid" style="border:1px solid #ccc; ">
+            </div>
+            <div class="col-md-6">
+                <h2>Create an account & Book your appointment</h2>
                 <p>
                     Lorem ipsum
                 </p>
@@ -15,7 +15,7 @@
                     <button class="btn btn-success">Register as Patient</button>
                     <button class="btn btn-secondary">Login</button>
                 </div>
-           </div>
+            </div>
         </div>
         <hr>
         <!--Search doctors-->
@@ -27,7 +27,7 @@
                 <div class="card-body">
                     <div class="row">
                         <div class="col-md-8">
-                            <input type="date" name="date" class="form-control">
+                            <input type="text" name="date" class="form-control" id="datepicker">
                         </div>
                         <div class="col-md-4">
                             <button class="btn btn-primary" type="submit">Find Doctors</button>
@@ -43,26 +43,26 @@
                 <div class="card-body">
                     <table class="table table-striped">
                         <thead>
-                            <tr>
-                                <th>#</th>
-                                <th>Photo</th>
-                                <th>Name</th>
-                                <th>Expertise</th>
-                                <th>Book</th>
-                            </tr>
+                        <tr>
+                            <th>#</th>
+                            <th>Photo</th>
+                            <th>Name</th>
+                            <th>Expertise</th>
+                            <th>Book</th>
+                        </tr>
                         </thead>
                         <tbody>
-                            <tr>
-                                <th scope="row">1</th>
-                                <td>
-                                    <img src="/doctor/doctor.jpg" width="100px" style="border-radius: 50%">
-                                </td>
-                                <td>Name of doctor</td>
-                                <td>Cardiologist</td>
-                                <td>
-                                    <button class="btn btn-success">Book Appointment</button>
-                                </td>
-                            </tr>
+                        <tr>
+                            <th scope="row">1</th>
+                            <td>
+                                <img src="/doctor/doctor.jpg" width="100px" style="border-radius: 50%">
+                            </td>
+                            <td>Name of doctor</td>
+                            <td>Cardiologist</td>
+                            <td>
+                                <button class="btn btn-success">Book Appointment</button>
+                            </td>
+                        </tr>
                         </tbody>
                     </table>
                 </div>
