4cb2bf13972310a7aa020677dbbc404014b30685
ivanovskipetar <ivanovskipetar4@gmail.com>
2024-03-10 15:27:36 +0100

    Initial design of welcome page.

--- doctor-app/routes/web.php
+++ doctor-app/routes/web.php
@@ -17,7 +17,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('dashboard');
+    return view('welcome');
 });
 
 Route::get('/dashboard', function () {
