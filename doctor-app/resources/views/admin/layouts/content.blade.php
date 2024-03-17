14a4fe21d9da71d68c3bcb59154253aef9723069
Бојковски Дамјан <damjan.bojkovski@students.finki.ukim.mk>
2024-03-17 16:08:26 +0000

    Booking branch

--- doctor-app/resources/views/admin/layouts/content.blade.php
+++ doctor-app/resources/views/admin/layouts/content.blade.php
@@ -10,7 +10,7 @@
                                         <div class="d-flex justify-content-between align-items-center">
                                             <div class="state">
                                                 <h6>Patients</h6>
-                                                <h2>{{\App\Models\User::where('role_id',2)->count()}}</h2>
+                                                <h2>{{App\Models\User::where('role_id',3)->count()}}</h2>
                                             </div>
                                             <div class="icon">
                                                 <i class="ik ik-users"></i>
@@ -29,7 +29,7 @@
                                         <div class="d-flex justify-content-between align-items-center">
                                             <div class="state">
                                                 <h6>Doctors</h6>
-                                                <h2>{{\App\Models\User::where('role_id',1)->count()}}</h2>
+                                                <h2>{{App\Models\User::where('role_id',1)->count()}}</h2>
                                             </div>
                                             <div class="icon">
                                                 <i class="ik ik-user-plus"></i>
@@ -48,7 +48,7 @@
                                         <div class="d-flex justify-content-between align-items-center">
                                             <div class="state">
                                                 <h6>Roles</h6>
-                                                <h2>{{\App\Models\Role::count()}}</h2>
+                                                <h2>{{App\Models\Role::count()}}</h2>
                                             </div>
                                             <div class="icon">
                                                 <i class="ik ik-user-check"></i>
@@ -67,7 +67,7 @@
                                         <div class="d-flex justify-content-between align-items-center">
                                             <div class="state">
                                                 <h6>Booking</h6>
-{{--                                                <h2>{{Booking::count()}}</h2>--}}
+                                                <h2>{{App\Models\Booking::count()}}</h2>
                                             </div>
                                             <div class="icon">
                                                 <i class="ik ik-message-square"></i>
@@ -81,48 +81,48 @@
                                 </div>
                             </div>
 
-                               <div class="col-lg-3 col-md-6 col-sm-12">
-                                <div class="widget">
-                                    <div class="widget-body">
-                                        <div class="d-flex justify-content-between align-items-center">
-                                            <div class="state">
-                                                <h6>Prescription</h6>
-{{--                                                <h2>{{Prescription::count()}}</h2>--}}
-                                            </div>
-                                            <div class="icon">
-                                                <i class="ik ik-align-justify"></i>
-                                            </div>
-                                        </div>
+{{--                               <div class="col-lg-3 col-md-6 col-sm-12">--}}
+{{--                                <div class="widget">--}}
+{{--                                    <div class="widget-body">--}}
+{{--                                        <div class="d-flex justify-content-between align-items-center">--}}
+{{--                                            <div class="state">--}}
+{{--                                                <h6>Prescription</h6>--}}
+{{--                                                <h2>{{App\Models\Prescription::count()}}</h2>--}}
+{{--                                            </div>--}}
+{{--                                            <div class="icon">--}}
+{{--                                                <i class="ik ik-align-justify"></i>--}}
+{{--                                            </div>--}}
+{{--                                        </div>--}}
 
-                                    </div>
-                                    <div class="progress progress-sm">
-                                        <div class="progress-bar bg-info" role="progressbar" aria-valuenow="20" aria-valuemin="0" aria-valuemax="100" style="width: 20%;"></div>
-                                    </div>
-                                </div>
-                            </div>
+{{--                                    </div>--}}
+{{--                                    <div class="progress progress-sm">--}}
+{{--                                        <div class="progress-bar bg-info" role="progressbar" aria-valuenow="20" aria-valuemin="0" aria-valuemax="100" style="width: 20%;"></div>--}}
+{{--                                    </div>--}}
+{{--                                </div>--}}
+{{--                            </div>--}}
 
-                        <div class="col-lg-3 col-md-6 col-sm-12">
-                                <div class="widget">
-                                    <div class="widget-body">
-                                        <div class="d-flex justify-content-between align-items-center">
-                                            <div class="state">
-                                                <h6>Department</h6>
-{{--                                                <h2>{{App\Department::count()}}</h2>--}}
-                                            </div>
-                                            <div class="icon">
-                                                <i class="ik ik-home"></i>
-                                            </div>
-                                        </div>
+{{--                        <div class="col-lg-3 col-md-6 col-sm-12">--}}
+{{--                                <div class="widget">--}}
+{{--                                    <div class="widget-body">--}}
+{{--                                        <div class="d-flex justify-content-between align-items-center">--}}
+{{--                                            <div class="state">--}}
+{{--                                                <h6>Department</h6>--}}
+{{--                                                <h2>{{App\Models\Department::count()}}</h2>--}}
+{{--                                            </div>--}}
+{{--                                            <div class="icon">--}}
+{{--                                                <i class="ik ik-home"></i>--}}
+{{--                                            </div>--}}
+{{--                                        </div>--}}
 
-                                    </div>
-                                    <div class="progress progress-sm">
-                                        <div class="progress-bar bg-info" role="progressbar" aria-valuenow="20" aria-valuemin="0" aria-valuemax="100" style="width: 20%;"></div>
-                                    </div>
-                                </div>
-                            </div>
+{{--                                    </div>--}}
+{{--                                    <div class="progress progress-sm">--}}
+{{--                                        <div class="progress-bar bg-info" role="progressbar" aria-valuenow="20" aria-valuemin="0" aria-valuemax="100" style="width: 20%;"></div>--}}
+{{--                                    </div>--}}
+{{--                                </div>--}}
+{{--                            </div>--}}
 
 
-                        </div>
+                     </div>
 
                     </div>
                 </div>
